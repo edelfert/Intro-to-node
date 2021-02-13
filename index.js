49d8b9372e68835a5e543183e4930ca87bb4b66c
Eddie <github.towage@aleeas.com>
2021-02-13 13:03:35 -0800

    first and only commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const superhero = require('superhero-name-library')
+const supervillains = require('supervillains');
+
+var superName = superhero.random();
+var villName = supervillains.random();
+
+console.log(superName)
+console.log(villName)
